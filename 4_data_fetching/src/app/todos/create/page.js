c9d516002543bc5d2374206f6adcf7ce32938321
dedlop <104914772+dedlop@users.noreply.github.com>
2025-01-09 12:00:09 -0300

    Iniciando Next com Prisma

---
+++ 4_data_fetching/src/app/todos/create/page.js
@@ -0,0 +1,9 @@
+const TodoPage = () => {
+  return (
+    <div className="max-w-md mx-auto mt-10">
+        <h1 className="text-2xl font-bold text-center mb-6">Criar nova tarefa</h1>
+    </div>
+  )
+}
+
+export default TodoPage
