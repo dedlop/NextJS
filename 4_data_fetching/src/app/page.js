435def6752e6e8f20fc7500ae2b32d5fd60ecae8
dedlop <104914772+dedlop@users.noreply.github.com>
2025-01-09 22:28:32 -0300

    Criação do formulário e algumas estilizações

--- 4_data_fetching/src/app/page.js
+++ 4_data_fetching/src/app/page.js
@@ -1,11 +1,47 @@
-import Image from "next/image";
+import { db } from "@/db";
+
 import Link from "next/link";
 
-export default function Home() {
-  return (
-    
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+export default async function Home() {
+  // 3 - Resgatando dados do banco
+  const todos = await db.todo.findMany();
+
+  return (    
+      <main className="container mx-auto p-4">
         <Link href="/todos/create">Ir para criação de todo</Link>
+        <h1 className="text-2xl font-bold mb-4">Todos!</h1>
+        <div className="space-y-4">
+          {todos.map((todo) => (
+            <div key={todo.id} className="bg-gray-100 p-4 rounded-lg shadow">
+              <div className="flex justify-between items-start">
+                <div>
+                  <h2 className="text-xl font-semibold">{todo.titulo}</h2>
+                  <p>{todo.descricao}</p>
+                </div>
+                <div className="flex space-x-2 mt-3">
+                  <Link
+                    href=""
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Visualizar
+                  </Link>
+                  <Link
+                    href=""
+                    className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    href=""
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     );
 }
--- 4_data_fetching/src/app/todos/create/page.js
+++ 4_data_fetching/src/app/todos/create/page.js
@@ -1,7 +1,67 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+// 1 - Criação form
 const TodoPage = () => {
+  // formData = useState
+  // 2 - Inserindo dados no banco
+  const addTodo = async(formData) => {
+    "use server";
+
+    console.log(formData);
+
+    const titulo = formData.get("titulo");
+    const descricao = formData.get("descricao");
+    const status = "pendente"
+
+    console.log(titulo, descricao);
+
+    const todo = await db.todo.create({
+      data: {
+        titulo,
+        descricao,
+        status,
+      },
+    });
+
+    console.log(todo);
+
+    redirect("/");
+  };
+
   return (
     <div className="max-w-md mx-auto mt-10">
         <h1 className="text-2xl font-bold text-center mb-6">Criar nova tarefa</h1>
+        <form
+          action={addTodo}
+          className="flex flex-col gap-4 p-4 bg-white shadow-lg rounded-lg">
+          <label htmlFor="titulo" className="block text-sm font-medium text-gray-700">
+            Título
+            <input 
+              type="text"
+              id="titulo"
+              name="titulo"
+              className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full"
+            />
+          </label>
+          <label htmlFor="descricao" className="block text-sm font-medium text-gray-700">
+            Descrição
+            <textarea 
+              type="text"
+              id="descricao"
+              name="descricao"
+              placeholder="Descreva a tarefa"
+              required
+              className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full h-32"
+            ></textarea>
+          </label>
+          <button 
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            >
+              Criar Todo
+            </button>
+        </form>
     </div>
   )
 }
