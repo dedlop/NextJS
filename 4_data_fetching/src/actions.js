8b843fe1bb13dfd497f12b83571407f8124864a4
dedlop <104914772+dedlop@users.noreply.github.com>
2025-01-16 11:22:25 -0300

    Conclusão do módulo de Fetch e Server Actions

--- 4_data_fetching/src/actions.js
+++ 4_data_fetching/src/actions.js
@@ -33,9 +33,46 @@ export const addTodo = async(formData) => {
 
 export const findTodoById = async (id) => {
 
+      // throw new Error("Ops!");
+
     const todo = db.todo.findFirst({
         where: {id},
     })
 
     return todo;
+};
+
+export const updateTodo = async(formState, formData) => {
+  const id = Number(formData.get("id"));
+  const titulo = formData.get("titulo");
+  const descricao = formData.get("descricao");
+
+  try {
+    // throw new Error("Falha ao salvar dados, sistema offline.");
+
+    if(titulo.length < 5) {
+      return {
+        errors: "O título precisa de pelo menos 5 caracteres.",
+      }
+    }
+  
+    if(descricao.length < 10) {
+      return {
+        errors: "A descrição precisa de pelo menos 10 caracteres.",
+      }
+    }
+
+    await db.todo.update({
+      where: { id },
+      data: { titulo, descricao },
+    });
+  
+    redirect("/");
+  } catch (error) {
+
+
+    return {
+      errors: error.message,
+    }
+  }
 };
--- 4_data_fetching/src/app/todos/[id]/edit/page.js
+++ 4_data_fetching/src/app/todos/[id]/edit/page.js
@@ -1,4 +1,5 @@
 import { findTodoById } from '@/actions'
+import TodoForm from '@/components/TodoForm'
 import React from 'react'
 
 const TodoEdit = async ({params}) => {
@@ -8,9 +9,11 @@ const TodoEdit = async ({params}) => {
     const todo = await findTodoById(id)
 
   return (
-    <div>
-        <h1>Editando: {todo.titulo}</h1>
-        {/* form */}
+    <div className="max-w-md mx-auto mt-10">
+        <h1 className="text-2xl font-bold text-center mb-6">
+          Editando: {todo.titulo}
+        </h1>
+        <TodoForm todo={todo} />
     </div>
   )
 }
